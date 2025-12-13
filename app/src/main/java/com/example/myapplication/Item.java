374d37848b7f264ad8d2a5d674e22b99ba756de1
Dyrexoo <zezoashrf66354@gmail.com>
2025-12-13 16:59:34 +0200

    Home page in progress

---
+++ app/src/main/java/com/example/myapplication/Item.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class Item {
+    String parkingName;
+
+    public Item(String parkingName) {
+        this.parkingName = parkingName;
+    }
+
+    public String getParkingName() {
+        return parkingName;
+    }
+
+    public void setParkingName(String parkingName) {
+        this.parkingName = parkingName;
+    }
+}
