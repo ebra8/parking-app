2bd3a0766f73f4fd9c7ef695db544fa7af26164d
Dyrexoo <zezoashrf66354@gmail.com>
2025-12-13 16:44:58 +0200

    Home page in progress

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.cardview)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
